66bccbe4a37325730eec933ae7dcaadea764d9cf
Aryan-Ark-02 <Arksharma1211@gmail.com>
2024-08-05 11:18:45 +0530

    weather

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+import requests
 from flask import Flask,render_template,request,redirect,session,url_for
 from DB import Database
-import json
 
 obj = Database()
 
@@ -15,6 +15,9 @@ def Signup():
 @app.route("/Login")
 def Login():
     return render_template('Login.html')
+@app.route("/Weather")
+def Weather():
+    return render_template('Weather.html')
 
 # SEP
 
@@ -38,5 +41,20 @@ def doSignup():
     else:
         return render_template('Login.html',msg ='Sign up successful' )
 
+@app.route("/weather-forecast",methods=['post'])
+def wetherForecast():
+    city = request.form.get('city')
+    api_key = '2f67c4bbcea52d586720123f8eb08ee3'    
+
+    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
+
+    mainWeather = weather_data.json()['weather'][0]['main']
+    weatherDescription = weather_data.json()['weather'][0]['description']
+    temp =  weather_data.json()['main']['temp']
+    feelsLike = weather_data.json()['main']['feels_like']
+    humidity = weather_data.json()['main']['humidity']
+
+    return render_template('present.html',mainWeather_=mainWeather,weatherDescription_=weatherDescription,temp_=temp,feelsLike_=feelsLike,humidity_=humidity,city_=city) 
+    
 app.run(debug=True)
 
