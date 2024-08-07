4f74679015f97b7114773717bcba51b297181975
Aryan-Ark-02 <Arksharma1211@gmail.com>
2024-08-07 22:28:42 +0530

    Meta and POS

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,render_template,request,redirect,session,url_for
 from DB import Database
 from PIL import Image
 from os import path
+import nltk
 
 obj = Database()
 
@@ -23,6 +24,9 @@ def Weather():
 @app.route("/m")
 def m():
     return render_template('m.html')
+@app.route("/pos")
+def POS():
+    return render_template('POS.html')
 
 # SEP
 
@@ -47,6 +51,41 @@ def upload():
         return render_template('m.html',type_=type,width_=width,height_=height,mode_=mode,jugad_Path_=jugad_Path,file_name=file_name,file_size=file_size)
 
 
+@app.route("/analyze", methods=['post'])
+def analyze():
+    textInput = request.form.get('textInput')
+    listed = nltk.word_tokenize(textInput)
+    usd = nltk.pos_tag(listed)
+    dictionary = {'NN':[],'PRP':[],'VV':[],'RB':[],'JJ':[],'CC':[],'UH':[],'IN':[],}
+    for i in usd:
+        if i[-1] == 'NN': #or i[-1] == 'NNS' or i[-1] == 'NNP' or i[-1] == 'NNPS':
+            if i[0] not in dictionary['NN']:
+                dictionary['NN'].append(i[0])
+        elif i[-1] == 'PRP' or i[-1] == 'PRP$':
+            if i[0] not in dictionary['PRP']:
+                dictionary['PRP'].append(i[0])
+        elif i[-1].startswith('V'):
+            if i[0] not in dictionary['VV']:
+                dictionary['VV'].append(i[0]) 
+        elif i[-1] == 'RB' or i[-1] == 'WRB' or i[-1] == 'RBR' or i[-1] == 'RBS' :
+            if i[0] not in dictionary['RB']:
+                dictionary['RB'].append(i[0])
+        elif i[-1].startswith('J'):
+            if i[0] not in dictionary['JJ']:
+                dictionary['JJ'].append(i[0])
+        elif i[-1] == 'CC':
+            if i[0] not in dictionary['CC']:
+                dictionary['CC'].append(i[0])
+        elif i[-1] == 'UH':
+            if i[0] not in dictionary['UH']:
+                dictionary['UH'].append(i[0])
+        elif i[-1] == 'IN':
+            if i[0] not in dictionary['IN']:
+                dictionary['IN'].append(i[0])
+        else: continue
+
+    return render_template('POS.html',textInput=textInput,dictionary=dictionary)
+    
 @app.route("/do-login", methods=['post'])
 def doLogin():
     email = request.form.get('email')
@@ -72,7 +111,7 @@ def wetherForecast():
     city = request.form.get('city')
     api_key = '2f67c4bbcea52d586720123f8eb08ee3'    
 
-    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
+    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={api_key}")
 
     mainWeather = weather_data.json()['weather'][0]['main']
     weatherDescription = weather_data.json()['weather'][0]['description']
